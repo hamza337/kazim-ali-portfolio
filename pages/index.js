62255d2c7a97497a0bffda9ac139121588494b47
Ali Hamza <hk72663@gmail.com>
2025-02-14 16:33:25 +0500

    seo changes 1.0.2

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,47 @@
-import dynamic from "next/dynamic";
+// import dynamic from "next/dynamic";
+// import ParentHOC from "../components/ParentHOC";
+// import HomeHorizontal from "../components/pages/home-horizontal";
+// import { useEffect, useState } from "react";
+// import { PopUp } from "../components/PopUp";
+// const index = () => {
+//   useEffect(() => {
+//     localStorage.setItem('popupShown', false);
+//     const handleBeforeUnload = () => {
+//       localStorage.setItem('popupShown', false);
+//     };
+//     window.addEventListener('beforeunload', handleBeforeUnload);
+//     return () => {
+//       window.removeEventListener('beforeunload', handleBeforeUnload);
+//     };
+//   }, []);
+
+//      const pageTitle = "Sir Syed Kazim Ali";
+//      const description = "Read what our students have to say about their experience with our coaching services.";
+//      const keywords = "student reviews, coaching, essay writing";
+//      const image = "https://syedkazimali.info/img/hero/kazim.jpg"; 
+
+
+
+//   return (
+//     <ParentHOC
+//       authorizedIgnored={true}
+//       pageTitle={pageTitle} 
+//       description={description} 
+//       keywords={keywords} 
+//       image={image}>
+//       <HomeHorizontal />
+      
+//     </ParentHOC>
+//   );
+// };
+
+// export default dynamic(() => Promise.resolve(index), { ssr: false });
+
 import ParentHOC from "../components/ParentHOC";
 import HomeHorizontal from "../components/pages/home-horizontal";
-import { useEffect, useState } from "react";
-import { PopUp } from "../components/PopUp";
-const index = () => {
+import { useEffect } from "react";
+
+const Index = () => {
   useEffect(() => {
     localStorage.setItem('popupShown', false);
     const handleBeforeUnload = () => {
@@ -15,12 +53,10 @@ const index = () => {
     };
   }, []);
 
-     const pageTitle = "Sir Syed Kazim Ali";
-     const description = "Read what our students have to say about their experience with our coaching services.";
-     const keywords = "student reviews, coaching, essay writing";
-     const image = "https://syedkazimali.info/img/hero/kazim.jpg"; 
-
-
+    const pageTitle = "Sir Syed Kazim Ali";
+    const description = "Read what our students have to say about their experience with our coaching services.";
+    const keywords = "student reviews, coaching, essay writing";
+    const image = "https://syedkazimali.info/img/hero/kazim.jpg";
 
   return (
     <ParentHOC
@@ -28,12 +64,11 @@ const index = () => {
       pageTitle={pageTitle} 
       description={description} 
       keywords={keywords} 
-      image={image}>
+      image={image}
+    >
       <HomeHorizontal />
-      
     </ParentHOC>
   );
 };
 
-export default dynamic(() => Promise.resolve(index), { ssr: false });
-
+export default Index;
